f6ad4981306d90fb3142b306639ed31c2653a891
TIMM SNOW <two.tone.snow@gmail.com>
2021-04-22 19:44:33 -0400

    seeds more experiences, adds description column and lat/lng columns to experience model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_155145) do
+ActiveRecord::Schema.define(version: 2021_04_22_222943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_04_21_155145) do
     t.datetime "updated_at", precision: 6, null: false
     t.decimal "lat", precision: 10, scale: 6
     t.decimal "lng", precision: 10, scale: 6
+    t.text "description"
   end
 
   create_table "list_items", force: :cascade do |t|
